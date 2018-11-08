f073206bb20e1f5e17931d9ac3e534f8a7a31999
Ivan Zlatoustov <ivan.zlatoustov@mariadb.com>
2018-11-08 14:50:20 +0200

    Fixed: 1. mariadb/server:10.3 2. sandbox load code

--- tx_sandbox/init.sh
+++ tx_sandbox/init.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cp -a /tmp/mysql /var/lib/
+mysql -uroot -p$MYSQL_ROOT_PASSWORD < /tmp/bookstore/csv/01_load_tx_init.sql
+mysql -uroot -p$MYSQL_ROOT_PASSWORD < /tmp/bookstore/csv/02_load_tx_ldi.sql
+mysql -uroot -p$MYSQL_ROOT_PASSWORD < /tmp/bookstore/csv/03_load_tx_verify.sql
 
