f547b64b4d4b16e7b8c40b254ca6512f659179e7
Ivan Zlatoustov <ivan.zlatoustov@mariadb.com>
2018-11-08 18:11:49 +0200

    init.sh is no longer needed

--- tx_sandbox/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mysql -uroot -p$MYSQL_ROOT_PASSWORD < /tmp/bookstore/csv/01_load_tx_init.sql
-mysql -uroot -p$MYSQL_ROOT_PASSWORD < /tmp/bookstore/csv/02_load_tx_ldi.sql
-mysql -uroot -p$MYSQL_ROOT_PASSWORD < /tmp/bookstore/csv/03_load_tx_verify.sql
-
